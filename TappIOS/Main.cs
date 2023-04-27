80590e23ff09c991071889ec57d943dcb81b1214
dgomez <xd43vild69@gmail.com>
2023-04-26 20:39:00 -0500

    t2.2

--- TappIOS/Main.cs
+++ TappIOS/Main.cs
@@ -5,7 +5,7 @@ namespace TappIOS
     public class Application
     {
         // This is the main entry point of the application.
-        // feature/test2.1
+        // feature/test2.2
         static void Main(string[] args)
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
