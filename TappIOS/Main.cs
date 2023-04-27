96d2f0c91b6ce0234df4406bf55c91c9aee8ff63
dgomez <xd43vild69@gmail.com>
2023-04-26 20:43:39 -0500

    m1

--- TappIOS/Main.cs
+++ TappIOS/Main.cs
@@ -4,11 +4,12 @@ namespace TappIOS
 {
     public class Application
     {
-        // This is the main entry point of the application.
+        // This is the main entry point of the application.        
         static void Main(string[] args)
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
+            // main v1
             UIApplication.Main(args, null, typeof(AppDelegate));
         }
     }
